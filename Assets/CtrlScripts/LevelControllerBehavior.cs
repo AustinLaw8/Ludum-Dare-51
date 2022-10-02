2cd95903445a3a6e445eb88c375343a5fc328e04
Joshua Denk <joshua.denk85@gmail.com>
2022-10-02 14:15:53 -0700

    Spawn code

--- Assets/CtrlScripts/LevelControllerBehavior.cs
+++ Assets/CtrlScripts/LevelControllerBehavior.cs
@@ -12,6 +12,10 @@ public class LevelControllerBehavior : MonoBehaviour
     public static LevelControllerBehavior levelController;
     public static Vector3 PLAYER_CENTER = new Vector3(.1f,-.1f,0);
     public static float PLAYER_RADIUS = .5f;
+    public static float MIN_SPAWN_DISTANCE = 12.5f;
+    public static int ENEMY_COUNT_INITIAL_SPAWN = 4;
+    public static float DIFFICULTY_FACTOR = 0.5f; // for now affects BOTH scaling of enmy spawn count per wave and enemy non-HP stats
+    public GameObject MELEE_ENEMY_PREFAB;
 
     public GameObject player;
     private PlayerBehavior _playerBehavior; public PlayerBehavior playerBehavior {get {return _playerBehavior;}}
@@ -19,9 +23,7 @@ public class LevelControllerBehavior : MonoBehaviour
     public Weapon currentWeapon;
 
     /* Weapon prefabs */
-    public GameObject SWORD_PREFAB;
-    public GameObject STAR_PREFAB;
-    public GameObject DYNAMITE_PREFAB;
+    public GameObject SWORD_PREFAB, STAR_PREFAB, DYNAMITE_PREFAB;
     public Dictionary<Weapon.WeaponType, GameObject> weaponPrefabs;
 
     // settings 
@@ -29,6 +31,7 @@ public class LevelControllerBehavior : MonoBehaviour
 
     private float _gameDuration, _gameDurationNextSwap;
     public float dimensionlessClampedTimeTilNextSwap {get {return Mathf.Clamp((10f -_gameDurationNextSwap + _gameDuration) / 10f, 0f, 1f);}}
+    public float swapCount {get {return Mathf.Floor(_gameDuration / 10f);}}
     void Awake()
     {
         if (LevelControllerBehavior.levelController) {
@@ -117,11 +120,12 @@ public class LevelControllerBehavior : MonoBehaviour
         _gameDuration = 0f;
         _gameDurationNextSwap = 10f;
         currentWeapon = GameObject.Instantiate(
-                weaponPrefabs[Weapon.WeaponType.DYNAMITE],
+                weaponPrefabs[Weapon.WeaponType.SWORD],
                 new Vector3(PLAYER_CENTER.x + PLAYER_RADIUS * (playerBehavior.facingLeft ? -1f : 1f), PLAYER_CENTER.y, 0f),
                 Quaternion.identity,
                 _playerBehavior.transform)
                 .GetComponent<Weapon>();
+        SpawnEnemyWave();
     }
 
     // Called by any gameObject with a SpriteRenderer in their Update to set their OrderInLayer to a value dependent on the lowest point of their y-position
@@ -136,6 +140,26 @@ public class LevelControllerBehavior : MonoBehaviour
     // Don't worry about updating next swap time, already handled in Update() above
     private void TenSecondSwap()
     {
-        
+        SpawnEnemyWave();
+    }
+
+    private void SpawnEnemyWave()
+    {
+        int enemiesToSpawn = Mathf.FloorToInt(ENEMY_COUNT_INITIAL_SPAWN + swapCount * DIFFICULTY_FACTOR * Random.Range(0.8f, 1.2f));
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            GameObject newEnemy = GameObject.Instantiate(MELEE_ENEMY_PREFAB) as GameObject;
+            newEnemy.transform.position = playerBehavior.transform.position + MIN_SPAWN_DISTANCE * Random.Range(1f, 3f) * Get2DUnitFromRadians(Random.Range(0f, 2 * Mathf.PI));
+            EnemyBehavior newEnemyBehavior = newEnemy.GetComponent<EnemyBehavior>();
+            float statScaleFactor = Mathf.Pow((1f + DIFFICULTY_FACTOR / 10f), swapCount) * Random.Range(0.8f, 1.2f);
+            newEnemyBehavior.maxHp *= statScaleFactor;
+            newEnemyBehavior.hp = newEnemyBehavior.maxHp;
+            newEnemyBehavior.speed += newEnemyBehavior.speed * DIFFICULTY_FACTOR / 20f * swapCount * Random.Range(0.8f, 1.2f);
+        }
+    }
+
+    private Vector3 Get2DUnitFromRadians(float radians)
+    {
+        return new Vector3(Mathf.Cos(radians), Mathf.Sin(radians), 0f);
     }
 }
